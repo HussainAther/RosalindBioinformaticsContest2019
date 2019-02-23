e54c5dbc116b4bf7fe492adbee211e187d61f05a
Syed Hussain Ather <shussainather@gmail.com>
2019-02-23 16:14:03 -0500

    first comment

---
+++ final1.py
@@ -0,0 +1,9 @@
+
+"""
+Cancer progression can be viewed as an accumulation of somatic mutations over the life of the cell, starting with a single cell with a
+healthy reference genome, and ultimately resulting in a cancerous tumor, where genome structure is severely altered. For the sake of
+simplicity, we can assume that the only changes that happen to the genome during cancer progression are large scale structural variations,
+and we will omit the facts that tumors can be heterogeneous, and that the genomes in questions are not haploid. With this simplified
+model in mind, we can try to solve the problem of reconstructing the sequential structure of derived chromosomes in cancer genomes.
+
+"""
