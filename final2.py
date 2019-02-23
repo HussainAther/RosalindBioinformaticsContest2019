f69caa4dfec2a762b2717643181e5407fd65bdae
Syed Hussain Ather <shussainather@gmail.com>
2019-02-23 16:26:48 -0500

    initial commit

---
+++ final2.py
@@ -0,0 +1,8 @@
+"""
+While having a constant nucleotide sequence DNA molecules in a cell can be chemically modified in a number of different
+ways. For example, the DNA bases can be methylated, or histone proteins, around which DNA is wrapped, can be supplied
+with chemical tags such as acetylation. It is thought that such modifications (or marks) define cell specialization
+or a cell state when a certain combination of marks regulate how genes are expressed.
+
+
+"""
