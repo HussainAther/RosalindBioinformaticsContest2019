6f7c46d2ade13bee096e27494ed1ad70db95787c
Syed Hussain Ather <shussainather@gmail.com>
2019-02-23 16:49:28 -0500

    comment

---
+++ final3.py
@@ -0,0 +1,11 @@
+
+
+"""
+While having a constant nucleotide sequence DNA molecules in a cell can be chemically modified in a number of different ways.
+For example, the DNA bases can be methylated, or histone proteins, around which DNA is wrapped, can be supplied with chemical
+tags such as acetylation. It is thought that such modifications (or marks) define cell specialization or a cell state when a
+certain combination of marks regulate how genes are expressed.
+
+In this problem, you are given genomic tracks describing a presence or an absence of several epigenomic marks. Your task is
+to split the genomic positions into a number of states so that each state corresponds to a particular combination of marks.
+"""
